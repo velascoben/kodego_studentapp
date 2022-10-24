c6a4832554e8fb1291e103010fed200d61cb17d0
benedict van velasco <velascobenedict@yahoo.com>
2022-10-24 15:31:15 +0800

    Sample UI

--- app/src/main/java/com/kodego/app/studentassistanceapp/TestActivity.kt
+++ app/src/main/java/com/kodego/app/studentassistanceapp/TestActivity.kt
@@ -2,10 +2,24 @@ package com.kodego.app.studentassistanceapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.kodego.app.studentassistanceapp.databinding.ActivityTestBinding
 
 class TestActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityTestBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_test)
+
+        binding = ActivityTestBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSubmit.setOnClickListener {
+//            Toast.makeText(applicationContext,"Hello from Toast",Toast.LENGTH_LONG).show()
+            var name : String = "Welcome Back, " + binding.etName.text.toString() + "!"
+            binding.txtName.text =  name
+        }
+
     }
 }
